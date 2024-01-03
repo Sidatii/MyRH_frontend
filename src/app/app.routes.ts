340064e37f3f94eebc970c84bed563af48bfea0a
sidatii <sidatnouhi@gmail.com>
2024-01-03 14:33:58 +0100

    form ...

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,5 +6,5 @@ import {OffersComponent} from "./pages/offers/offers.component";
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'offers', component: OffersComponent },
-  { path: 'apply/:id', component: ApplyComponent },
+  { path: 'offers/apply/:id', component: ApplyComponent },
 ];
